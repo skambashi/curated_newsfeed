b23ef2d36a08bec9e14e889696037f2aff338db8
Shayan Masood <zmasood.shayan@gmail.com>
2015-02-06 20:15:27 -0500

    Frank

---
+++ src/main/scala/Frank.scala
@@ -0,0 +1,23 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import scala.xml.XML
+
+object Frank {
+
+	def main(args: Array[String]) { 
+
+		
+		val conf = new SparkConf().setAppName("Chrysler Data")
+		.set("spark.executor.memory", "13g")
+		.set("spark.storage.memoryFraction", "0.3")
+		.set("spark.shuffle.memoryFraction", "0.7")
+		.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+		.set("spark.eventLog.enabled", "true")
+		.set("spark.eventLog.dir", "/home/hadoop/spark/logs/")
+		.set("spark.core.containsxection.ack.wait.timeout", "12000") /* 20 minutes */
+		.set("spark.shuffle.manager", "sort")
+		val sc = new SparkContext(conf)
+		
+	}
+}

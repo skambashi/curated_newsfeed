48393943e2ca60ba56457877837a9fe6e9a6d8e2
Seikun Kambashi <seikun_lee@hotmail.com>
2015-02-06 21:07:25 -0500

    parser interim

--- src/main/Parser.scala
+++ src/main/Parser.scala
@@ -2,16 +2,32 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import scala.xml.XML
+import scala.collection.mutable.ListBuffer
 
 object Feeder {
 
-	def parseFile (uri : String) : Array[String] = {
-		val feed = XML.load("/Users/shayanmasood/projects/frank/utils/sources.xml") \\ "outline" \\ "outline" \\ "@xmlUrl"
-		return feed.toArray
+	def parseFile () : Seq[String] = {
+		val urls = XML.load("/Users/skambashi/Desktop/Projects/curated_newsfeed/utils/sources.xml") \\ "outline" \\ "outline" \\ "@xmlUrl"
+		var sources = new ListBuffer[String]()
+		for (url <- urls) {
+			sources += (url text)
+		}
+		return sources.toSeq
+	}
+
+	def parseFeed (source : String) : Seq[Tuple3[String,String,String]] = {
+		val items = XML.load(source) \\ "item"
+		var articles = new ListBuffer[Tuple3[String,String,String]]
+		for (item <- items) {
+			val title = (item \\ "title").toString
+			val desc = (item \\ "description").toString
+			val link = (item \\ "link").toString
+			articles += Tuple3(title,desc,link)
+		}
+		return items.toSeq
 	}
 
 	def main(args: Array[String]) { 
-		
-				
+		val sources = parseFile().map(source => parseFeed(source))
 	}
 }
--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -5,14 +5,13 @@ import scala.xml.XML
 
 object Feeder {
 
-	def parseFile (uri : String) : Map[String] = 
-		val source = scala.io.Source.fromFile(uri)
-		val lines = source.mkString
-		source.close()
+	def parseFile (uri : String) : Array[String] = {
+		val feed = XML.load("/Users/shayanmasood/projects/frank/utils/sources.xml") \\ "outline" \\ "outline" \\ "@xmlUrl"
+		return feed.toArray
 	}
 
 	def main(args: Array[String]) { 
-
+		
 				
 	}
 }

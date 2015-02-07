641096800d797063d084bf6b358462e2b0d1f2bd
Seikun Kambashi <seikun_lee@hotmail.com>
2015-02-07 03:31:56 -0500

    todo: move feeder file write to method in parser.scala

--- src/main/scala/Frank.scala
+++ src/main/scala/Frank.scala
@@ -1,23 +1,34 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import scala.xml.XML
+// import org.apache.spark.SparkContext
+// import org.apache.spark.SparkContext._
+// import org.apache.spark.SparkConf
+import curated_newsfeed.Feeder
+import java.io._
 
 object Frank {
 
 	def main(args: Array[String]) { 
+		// val conf = new SparkConf().setAppName("Frank")
+		// .set("spark.executor.memory", "13g")
+		// .set("spark.storage.memoryFraction", "0.3")
+		// .set("spark.shuffle.memoryFraction", "0.7")
+		// .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+		// .set("spark.eventLog.enabled", "true")
+		// .set("spark.eventLog.dir", "/home/hadoop/spark/logs/")
+		// .set("spark.core.containsxection.ack.wait.timeout", "12000") /* 20 minutes */
+		// .set("spark.shuffle.manager", "sort")
+		// val sc = new SparkContext(conf)
+		
+		val test = Feeder
+		val sources = test.getArticles()
+		// sources.foreach(el => { el.foreach(in => println(in._1)) })
 
-		
-		val conf = new SparkConf().setAppName("Frank")
-		.set("spark.executor.memory", "13g")
-		.set("spark.storage.memoryFraction", "0.3")
-		.set("spark.shuffle.memoryFraction", "0.7")
-		.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-		.set("spark.eventLog.enabled", "true")
-		.set("spark.eventLog.dir", "/home/hadoop/spark/logs/")
-		.set("spark.core.containsxection.ack.wait.timeout", "12000") /* 20 minutes */
-		.set("spark.shuffle.manager", "sort")
-		val sc = new SparkContext(conf)
-		
+		val writer = new PrintWriter(new File("articles.txt"))
+		sources.foreach(source => {
+			source.foreach(article => {
+				writer.write(article._1 + "|" + article._2 + "|" + article._3)
+				writer.write("\n")
+			})
+		})
+		writer.close()
 	}
 }
--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,39 +1,42 @@
 import scala.xml.XML
 import scala.collection.mutable.ListBuffer
 
-package object Feeder {
+package curated_newsfeed {
+	object Feeder {
 
-	def parseFile () : Seq[String] = {
-		val urls = XML.load("/Users/skambashi/Desktop/Projects/curated_newsfeed/utils/sources.xml") \\ "@xmlUrl"
-		var sources = new ListBuffer[String]()
-		for (url <- urls) {
-			sources += (url text)
+		def parseFile () : Seq[String] = {
+			val urls = XML.load("/Users/skambashi/Desktop/Projects/curated_newsfeed/utils/sources.xml") \\ "@xmlUrl"
+			var sources = new ListBuffer[String]()
+			for (url <- urls) {
+				sources += url.text
+			}
+			return sources.toSeq
 		}
-		return sources.toSeq
-	}
 
-	def parseFeed (source : String) : Seq[Tuple3[String,String,String]] = {
-		val items = XML.load(source) \\ "item"
-		var articles = new ListBuffer[Tuple3[String,String,String]]
-		for (item <- items) {
-			val title = (item \\ "title" text)
-			val desc = (item \\ "description" text)
-			val link = (item \\ "link" text)
-			articles += Tuple3(title,desc,link)
+		def parseFeed (source : String) : Seq[Tuple3[String,String,String]] = {
+			val items = XML.load(source) \\ "item"
+			var articles = new ListBuffer[Tuple3[String,String,String]]
+			for (item <- items) {
+				val title = (item \\ "title").text.trim()
+				val pubDate = (item \\ "pubDate").text.trim()
+				val link = (item \\ "link").text.trim()
+				articles += Tuple3(title,pubDate,link)
+			}
+			if (articles.length < 5) {
+				println("WARNING | source is shit : " + source)
+			}
+			return articles.toSeq
 		}
-		if (articles.length < 5) {
-			println("WARNING | source is shit : " + source)
-		}
-		return articles.toSeq
-	}
 
-	def getArticles() : Seq[Seq[Tuple3[String,String,String]]] = {
-		val sources = parseFile()
-		val articles = new ListBuffer[Seq[Tuple3[String,String,String]]]
-		for(source <- sources) {
-			try {articles += parseFeed(source)}
-			catch {case e : Exception => println("Exception while reading sources : " + e + "\nURL : " + source)}
+		def getArticles() : Seq[Seq[Tuple3[String,String,String]]] = {
+			val sources = parseFile()
+			val articles = new ListBuffer[Seq[Tuple3[String,String,String]]]
+			for(source <- sources) {
+				try {articles += parseFeed(source)}
+				catch {case e : Exception => println("Exception while reading sources : " + e + "\nURL : " + source)}
+			}
+			return articles.toSeq
 		}
-		return articles.toSeq
+
 	}
 }

356ebc88e4666bcb7a81752592f22fe5ecfc8c0d
Shayan Masood <zmasood.shayan@gmail.com>
2015-02-12 22:53:25 -0500

    Format

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -7,7 +7,7 @@ package curated_newsfeed {
   object Feeder {
 
     var bad_sources = new ListBuffer[String]
-    val source_uri= "/Users/shayanmasood/projects/frank/utils/sources.xml"
+    val source_uri = "/Users/shayanmasood/projects/frank/utils/sources.xml"
     val bad_source_uri = "/Users/shayanmasood/projects/frank/utils/bad_sources.txt"
 
     def using[A <: { def close(): Unit }, B](param: A)(f: A => B): B =
@@ -21,14 +21,14 @@ package curated_newsfeed {
           }
       }
     }
-
+    
     //Read XML RSS feeds
     def parseFile(): Seq[String] = {
       val urls = XML.load(source_uri) \\ "@xmlUrl"
       var sources = new ListBuffer[String]()
       for (url <- urls) {
         if (!bad_sources.contains(url)) {
-         	 sources += url.text
+          sources += url.text
         }
       }
       return sources.toSeq
@@ -46,32 +46,32 @@ package curated_newsfeed {
       }
       if (articles.length < 5) {
         println("WARNING | source is shit : " + source)
-        appendToFile(bad_source_uri, articles(0)._3 )
+        appendToFile(bad_source_uri, articles(0)._3)
       }
       return articles.toSeq
     }
 
     //Load bad sources logged previously to prevent 403s/404s/401s
     def getBadSources() {
-			for (line <- Source.fromFile(bad_source_uri).getLines()) {
-			  bad_sources += line.mkString
-			}
+      for (line <- Source.fromFile(bad_source_uri).getLines()) {
+        bad_sources += line.mkString
+      }
     }
 
     //Get all articles	
     def getArticles(): Seq[Seq[Tuple3[String, String, String]]] = {
-    	getBadSources()
+      getBadSources()
       val sources = parseFile()
       val articles = new ListBuffer[Seq[Tuple3[String, String, String]]]
       for (source <- sources) {
         try { articles += parseFeed(source) }
         catch {
-         case e: Exception => 
-         			println("Exception while reading sources : " + e + "\nURL : " + source)
-        			bad_sources += source
-			        appendToFile(bad_source_uri, source)
+          case e: Exception =>
+            println("Exception while reading sources : " + e + "\nURL : " + source)
+            bad_sources += source
+            appendToFile(bad_source_uri, source)
         }
-     }
+      }
       return articles.toSeq
     }
   }

9c94d9acdf5c088fe1f025f142560d9ee5340788
Shayan Masood <zmasood.shayan@gmail.com>
2015-03-22 17:24:13 -0400

    lda interim

--- src/main/scala/Frank.scala
+++ src/main/scala/Frank.scala
@@ -3,6 +3,7 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.clustering.LDA
 import org.apache.spark.mllib.linalg.Vectors
+import collection.mutable.HashMap
 import curated_newsfeed.Feeder
 
 
@@ -13,25 +14,44 @@ object Frank {
     val sc = new SparkContext(conf)
     
     val feed = Feeder
-    val articles = sc.parallelize(feed.getArticles().zipWithIndex.map(_.swap))
-    val all_words = articles.flatMap(line =>line._2._4.trim().split("\\s+",-1)).distinct()
+    val articles = feed.getArticles().zipWithIndex.map(_.swap)
+    val all_words = articles.flatMap(line =>line._2._1.trim().split("\\s+",-1))
+      .filter(word=> {
+        val special_chars = "[^\\dA-Za-z ]".r
+        if ((special_chars findFirstIn word) != None) false else true
+        }).distinct.toSeq
 
     // Word count each description
-    val wc_feed = articles.map (r => {
+    val wc_feed = sc.parallelize(articles.map (r => {
       val index = r._1
-      val description = r._2._4.trim().split("\\s+",-1).toSeq
+      val description = r._2._1.trim().split("\\s+",-1).toSeq
       val word_count = all_words.map(word => {
         description.filter(el => (el == word)).length.toDouble
-      })
-      word_count
-      //description
-      // (index, Vectors.dense(word_count))
+      }).toArray
+      (index.toLong, Vectors.dense(word_count))
+    }))
+
+    val id_article_map = new HashMap[Int,Tuple4[String,String,String,String]]()
+    articles.map (r => {
+      id_article_map += Tuple2(r._1,r._2)
     })
 
     val ldaModel = new LDA().setK(10).run(wc_feed)
     println("Learned topics (as distributions over vocab of " + ldaModel.vocabSize + " words):")
     val topics = ldaModel.topicDistributions
-    
+      .map(r => {
+        val doc_id = r._1
+        val vec = r._2
+        var topic_id = -1
+        for (i <- 0 to 9) {
+          if (vec(i) > 0.3) {
+            topic_id = i
+          }
+        }
+        (topic_id, List(r._1))
+      })
+      .filter(r => r._1 != -1)
+      .reduceByKey((a,b) => (a ++ b))
   }
 }
 
